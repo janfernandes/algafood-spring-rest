454775d8f916ff0d35d796a00a8a540dbe021eca
Janayna Moura <fernandesmjanayna@gmail.com>
2020-02-14 10:09:01 -0300

    finalizando o desafio 11.20

--- src/main/java/com/algafood/api/model/mixin/CidadeMixin.java
+++ src/main/java/com/algafood/api/model/mixin/CidadeMixin.java
@@ -1,10 +1,10 @@
-package com.algafood.api.model.mixin;
-
-import com.algafood.domain.model.Estado;
-import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
-
-public class CidadeMixin {
-
-    @JsonIgnoreProperties(value = "nome", allowGetters = true)
-    private Estado estado;
-}
+//package com.algafood.api.model.mixin;
+//
+//import com.algafood.domain.model.Estado;
+//import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+//
+//public class CidadeMixin {
+//
+//    @JsonIgnoreProperties(value = "nome", allowGetters = true)
+//    private Estado estado;
+//}
--- src/main/java/com/algafood/api/model/mixin/CozinhaMixin.java
+++ src/main/java/com/algafood/api/model/mixin/CozinhaMixin.java
@@ -1,12 +1,12 @@
-package com.algafood.api.model.mixin;
-
-import com.algafood.domain.model.Restaurante;
-import com.fasterxml.jackson.annotation.JsonIgnore;
-
-import java.util.ArrayList;
-import java.util.List;
-
-public class CozinhaMixin {
-    @JsonIgnore
-    private List<Restaurante> restaurantes = new ArrayList<>();
-}
+//package com.algafood.api.model.mixin;
+//
+//import com.algafood.domain.model.Restaurante;
+//import com.fasterxml.jackson.annotation.JsonIgnore;
+//
+//import java.util.ArrayList;
+//import java.util.List;
+//
+//public class CozinhaMixin {
+//    @JsonIgnore
+//    private List<Restaurante> restaurantes = new ArrayList<>();
+//}
