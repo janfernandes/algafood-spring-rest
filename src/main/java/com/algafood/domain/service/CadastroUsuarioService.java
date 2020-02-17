23392e4fdf2a244f786f3cb7b571c5cf10a52060
Janayna Moura Fernandes <fernandesmjanayna@gmail.com>
2020-02-16 21:32:48 -0300

    adicionando até a aula 12.11

--- src/main/java/com/algafood/domain/service/CadastroUsuarioService.java
+++ src/main/java/com/algafood/domain/service/CadastroUsuarioService.java
@@ -8,6 +8,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.Optional;
+
 @Service
 public class CadastroUsuarioService {
     @Autowired
@@ -19,7 +21,21 @@ public class CadastroUsuarioService {
 
     @Transactional
     public Usuario salvar(Usuario usuario) {
+        usuarioRepository.detach(usuario);
+
+        verificarSeEmailJaCadastradoNaBase(usuario);
+
         return usuarioRepository.save(usuario);
+    }
+
+    private void verificarSeEmailJaCadastradoNaBase(Usuario usuario) {
+        Optional<Usuario> usuarioExistente = usuarioRepository.findByEmail(usuario.getEmail());
+
+        if(usuarioExistente.isPresent() && !usuarioExistente.get().equals(usuario)){
+            throw new NegocioException(
+                    String.format("Já existe um usuário cadastrado com o e-mail %s",
+                    usuario.getEmail()));
+        }
     }
 
     @Transactional
